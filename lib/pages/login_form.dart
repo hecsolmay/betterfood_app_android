ee3419d282ec591d046bb0aafea93f75a8c7b0c6
heyitsgaspar <israel.chay.32@gmail.com>
2023-02-07 17:50:03 -0600

    cambios login

--- lib/pages/login_form.dart
+++ lib/pages/login_form.dart
@@ -6,46 +6,52 @@ class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage(
-                'assets/images/FondoImg.jpg',
+          SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height,
               ),
-              fit: BoxFit.cover,
-            )),
+              child: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/Fondo.png'),
+                        fit: BoxFit.cover)),
+              ),
+            ),
           ),
-          ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            children: <Widget>[
-              // const SizedBox(height: 20.0
-              // ),
-              Column(
-                children: <Widget>[
-                  Image.asset(
-                    'assets/images/logo.png',
-                    width: 180,
-                    height: 180,
-                  ),
-                  const SizedBox(height: 230.0),
-                ],
-              ),
 
-              //const SizedBox(height: 120.0),
-              SizedBox(
-                height: 300,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
-                  elevation: 0,
-                  color: Colors.white,
+           Positioned(
+            top: 53,
+            left: 95,
+            child: Container(
+              width: 180,
+              height: 180,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/logo.png', 
+                  ),
+                  fit: BoxFit.fitWidth
+                ),
+              )
+            )
+          ),
+
+          SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.only(top: 450),
+              height: 300,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.all(40.0),
                   child: Column(
                     children: [
-                      const SizedBox(
-                        height: 30,
-                      ),
                       const Text(
                         'Bienvenido a BetterFood',
                         style: TextStyle(
@@ -55,47 +61,40 @@ class LoginForm extends StatelessWidget {
                         ),
                         textAlign: TextAlign.center,
                       ),
-                      const SizedBox(
-                        height: 45,
+                      // ignore: prefer_const_constructors
+                      SizedBox(
+                        height: 30,
                       ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 40),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            filled: true,
-                            labelText: 'Numero De Mesa:',
-                            fillColor: Color.fromARGB(255, 255, 255, 255),
-                            labelStyle: TextStyle(
-                                color: Color.fromRGBO(255, 131, 17, 100)),
-                          ),
-                          obscureText: true,
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Número de mesa',
+                          labelStyle: TextStyle(
+                              color: Color.fromRGBO(255, 131, 17, 100)),
                         ),
                       ),
-                      const SizedBox(height: 60),
-                      TextButton(
-                        style: TextButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            textStyle: const TextStyle(fontSize: 20),
-                            backgroundColor: const Color.fromRGBO(186, 0, 0, 1),
-                            fixedSize: const Size(280, 20),
-                            alignment: Alignment.bottomCenter),
+                      // ignore: prefer_const_constructors
+                      SizedBox(height: 50),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromRGBO(186, 0, 0, 1),
+                            fixedSize: const Size(300, 40)),
                         onPressed: () {
-                          // Navigator.pushNamed(context, '/home');
-                          Navigator.pushNamedAndRemoveUntil(
+                           Navigator.pushNamedAndRemoveUntil(
                             context,
                             '/home',
                             (route) => false,
                           );
                         },
-                        child: const Text('Enviar'),
-                      )
+                        child: const Text('Ingresar'),
+                      ),
                     ],
                   ),
                 ),
               ),
-              // spacer
-            ],
-          ),
+            ),
+          )
+
         ],
       ),
     );
