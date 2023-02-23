478f33391807e687b83ace73c6a2a345cfe66aec
HectorSolis <hecsolmay@gmail.com>
2023-02-22 20:52:18 -0600

    Merge from origin main

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,24 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const Login(),
-        '/home': (context) => const Home(),
-        '/register-form': (context) => const LoginForm(),
-        '/categories': (context) => Categories(),
-        '/order': (context) => const Order(),
-      },
+    return ChangeNotifierProvider(
+      create: (context) => CategoryProvider()..fetchCategory(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)),
+        initialRoute: '/login',
+        routes: {
+          '/login': (context) => const Login(),
+          '/home': (context) => const Home(),
+          '/register-form': (context) => const LoginForm(),
+          '/categories': (context) => const Categories(),
+          '/order': (context) => const Order(),
+        },
+      ),
     );
   }
-
-
 }
-
