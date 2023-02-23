61a5d2bd99bcceb6bf89bbf16fc53914b100646d
HectorSolis <hecsolmay@gmail.com>
2023-02-22 21:09:37 -0600

    Agregando variables globales

--- lib/dtos/providers/categoryprovider.dart
+++ lib/dtos/providers/categoryprovider.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:betterfood_app_android/common/globals.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
 import 'package:betterfood_app_android/dtos/response/categoryresponse.dart';
-class CategoryProvider extends ChangeNotifier{
+
+class CategoryProvider extends ChangeNotifier {
   bool isLoading = true;
   final logger = Logger();
 
@@ -12,26 +14,21 @@ class CategoryProvider extends ChangeNotifier{
 
   List<CategoryResponseDto>? get categories => _categories;
 
-   Future fetchCategory() async {
-    final response = await http
-      .get(Uri.parse('https://betterfood-api.up.railway.app/api/m/category'));
-     
-      
-  if (response.statusCode == 200) {
-    final json = jsonDecode(response.body);
-    final List<dynamic>results = json['results'];
-    print('${results}');
-    _categories = results.map((e) => CategoryResponseDto.fromMap(e)).toList();
-    isLoading = false;
+  Future fetchCategory() async {
+    final response =
+        await http.get(Uri.parse('${Globals.apiURL}/api/m/category'));
 
-    notifyListeners();
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      final List<dynamic> results = json['results'];
+      _categories = results.map((e) => CategoryResponseDto.fromMap(e)).toList();
+      isLoading = false;
+
+      notifyListeners();
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
   }
-  
-
-  
-}
 }
