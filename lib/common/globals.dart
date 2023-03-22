72476c67b3f6175bf96ac6d9cead07b4f05d1137
hecsolmay <hecsolmay@gmail.com>
2023-03-21 23:44:18 -0600

    cambiando url de la api

--- lib/common/globals.dart
+++ lib/common/globals.dart
@@ -1,4 +1,5 @@
 class Globals {
-  static String apiURL = 'https://betterfood-api.up.railway.app';
+  // static String apiURL = 'https://betterfood-api.up.railway.app';
+  static String apiURL = 'https://betterfood.onrender.com';
   // static String apiURL = 'http://192.168.0.104:3000';
 }
